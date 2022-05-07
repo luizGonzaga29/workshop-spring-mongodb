919208bfea3a0ed554571742ca41fa8871e98fdd
luizGonzaga29 <luizgonzagacabral29@gmail.comgit config --global user.email luizgonzagacabral29@gmail.com>
2022-05-07 19:26:27 -0300

    Conectando ao MongoDB repository e service

---
+++ src/main/java/com/luizgonzaga/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.luizgonzaga.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luizgonzaga.workshopmongo.domain.User;
+import com.luizgonzaga.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
