c085497df8be9ccc1adcfec1d03025d66d9a6c60
luizGonzaga29 <luizgonzagacabral29@gmail.comgit config --global user.email luizgonzagacabral29@gmail.com>
2022-05-15 15:58:39 -0300

    Obtendo post por id

---
+++ src/main/java/com/luizgonzaga/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.luizgonzaga.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luizgonzaga.workshopmongo.domain.Post;
+import com.luizgonzaga.workshopmongo.repository.PostRepository;
+import com.luizgonzaga.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
